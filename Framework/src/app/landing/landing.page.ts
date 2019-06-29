10b4e3d9c0286d53d7e2f6ceb6c4bcb45c84e33e
Mario Ochoa <mario8ato@hotmail.com>
2019-06-29 16:50:38 -0500

    otro desmadre

--- Framework/src/app/landing/landing.page.ts
+++ Framework/src/app/landing/landing.page.ts
@@ -9,45 +9,70 @@ import { NFC, Ndef } from '@ionic-native/nfc/ngx';
 })
 export class LandingPage implements OnInit {
 
+  // // tagId: any;
+
   constructor(private nfc: NFC, private ndef: Ndef, public toastController: ToastController) { }
 
   ngOnInit() {
-    
+    // this.tagId = this.addListenNFC();
   }
 
   async presentToast() {
     const toast = await this.toastController.create({
-      message: "no",
+      message: 'Nel jjajaja',
       duration: 2000
     });
     toast.present();
   }
 
-  async presentToastsi() {
-    const toast = await this.toastController.create({
-      message: "si",
-      duration: 2000
-    });
-    toast.present();
-  }
+  // async presentToastsi() {
+  //   const toast = await this.toastController.create({
+  //     message: "si",
+  //     duration: 2000
+  //   });
+  //   toast.present();
+  // }
 
-  addListenNFC() {
-    console.log('entra a addListenNFC');
+  // addListenNFC() {
+  //   console.log('entra a addListenNFC');
 
-    this.nfc.addNdefListener(() => {
-      console.log('successfully attached ndef listener');
-    }, (err) => {
-      console.log('error attaching ndef listener', err);
+  //   this.nfc.addNdefListener(() => {
+  //     console.log('successfully attached ndef listener');
+  //   }, (err) => {
+  //     console.log('error attaching ndef listener', err);
 
-    this.presentToast();
+  //   this.presentToast();
 
-    }).subscribe((event) => {
-      console.log('received ndef message. the tag contains: ', event.tag);
-      console.log('decoded tag id', this.nfc.bytesToHexString(event.tag.id));
+  //   }).subscribe((event) => {
+  //     console.log('received ndef message. the tag contains: ', event.tag);
+  //     console.log('decoded tag id', this.nfc.bytesToHexString(event.tag.id));
 
-      this.presentToastsi();
-    });
+  //     this.presentToastsi();
+  //   });
 
+  // }
+
+  // NFC IONIC 4
+
+
+
+  onClick() {
+
+    this.nfc.beginSession().subscribe(res => {
+      this.nfc.addNdefListener(() => {
+        console.log('successfully attached ndef listener');
+      }, (err) => {
+        console.log('error attaching ndef listener', err);
+        this.presentToast();
+      }).subscribe((event) => {
+        console.log('received ndef message. the tag contains: ', event.tag);
+        console.log('decoded tag id', this.nfc.bytesToHexString(event.tag.id));
+        this.presentToast();
+      });
+    },
+      err => {
+      console.log(err);
+      });
   }
 
 }
